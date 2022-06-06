9e4708ca65eb47ed697249c795e76fd48ce11d81
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2022-06-06 10:36:06 -0300

    removendo componente

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <!-- <Sidebar/>
+    <Sidebar/>
     <TitlePage/>
-    -->
-    <Header/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-  // import Sidebar from "./components/Sidebar.vue";
-  // import TitlePage from './components/TitlePage.vue';
+  import Sidebar from "./components/Sidebar.vue";
+  import TitlePage from './components/TitlePage.vue';
 
   export default {
     name: "App",
     components: {
-      // Sidebar,
-      // TitlePage,
-        Header,
+      Sidebar,
+      TitlePage,
     },
   };
 </script>
