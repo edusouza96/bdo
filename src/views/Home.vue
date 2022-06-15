f11fd3eaead428cf31f39ca0cdff19521676b453
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2022-06-15 08:42:43 -0300

    adicionando dobras de footer e links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,11 @@
     <header class="masterhead">
         <div class="container">
           <div class="row">
-            <div class="col-xs-12 col-lg-6 text-xs-center text-sm-center text-md-center text-lg-start">
-              <img src="../assets/logo.png" alt="logo banco de oportunidades" class="masterhead-image">
+            <div class="col-xs-12 col-lg-5 text-xs-center text-sm-center text-md-center text-lg-start">
+              <img src="../assets/logo.png" alt="logo banco de oportunidades" class="img-fluid">
             </div>
-            <div class="col-xs-12 col-lg-6 text-xs-center text-sm-center text-md-center text-lg-end">
-              <img src="../assets/brasao.png" alt="brasao prefeitura de canoas" class="masterhead-image mt-5">
+            <div class="col-xs-12 col-lg-7 text-xs-center text-sm-center text-md-center text-lg-end">
+              <img src="../assets/brasao.png" alt="brasao prefeitura de canoas" class="img-fluid mt-5">
             </div>
           </div>
         </div>
@@ -77,15 +77,15 @@
           <div class="carousel-item" v-for="(vacancy, index) in vacancies" :key="vacancy.id" :class="index == 0 ? 'active' : ''" >
             <div class="container">
               <div class="carousel-caption text-start">
-                <h2>{{ vacancy.title }}</h2>
+                <h2 class="text-truncate">{{ vacancy.title }}</h2>
                 <hr class="text-canoas-secondary">
                 <p><strong>Descrição</strong></p>
-                <p>{{ vacancy.description }}</p>
+                <p class="carousel-vacancies-description">{{ vacancy.description }}</p>
                 <p>
                   <strong>Nº de Vagas</strong>
                   {{ vacancy.numberJobsOpportunities }}
                 </p>
-                <p class="text-end"><a class="btn btn-lg btn-outline-light" href="#">Veja mais</a></p>
+                <p class="text-end"><a class="btn btn-lg btn-outline-success" href="#">Veja mais</a></p>
               </div>
             </div>
           </div>
@@ -118,7 +118,89 @@
       </div>
     </section>
     
+    <section class="page-section area-user">
+      <div class="container">
+        <h1 class="page-section-title">
+          Faça seu cadastro
+        </h1>
+        <div class="row mt-5 d-flex justify-content-center">
+          <div class="col-xs-12 col-md-6">
+            <div class="area-user-item">
+              <div class="row">
+                <div class="col-12 mt-4 text-center">
+                  <i class="fa fa-3x fa-user"></i> 
+                  <h3> Cidadão</h3>
+                </div>
+              </div>
+              <hr class="text-canoas-secondary">
+              <div class="row container">
+                <div class="d-grid gap-2">
+                  <button class="btn btn-success" type="button">Cadastrar</button>
+                  <button class="btn btn-success" type="button">Login</button>
+                </div>
+              </div>
 
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <div class="area-user-item">
+              <div class="row">
+                <div class="col-12 mt-4 text-center">
+                  <i class="fa fa-3x fa-building"></i> 
+                  <h3> Empresa</h3>
+                </div>
+              </div>
+              <hr class="text-canoas-secondary">
+              <div class="row container">
+                <div class="d-grid gap-2">
+                  <button class="btn btn-success" type="button">Cadastrar</button>
+                  <button class="btn btn-success" type="button">Login</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+
+
+    <footer class="footer py-4 text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 my-3 my-md-0">
+            <a class="btn btn-outline-light btn-social mx-2" target="_blank" href="https://www.youtube.com/c/PrefeituradeCanoasOficial" aria-label="YouTube"><i class="fab fa-youtube fa-2x"></i></a>
+            <a class="btn btn-outline-light btn-social mx-2" target="_blank" href="https://twitter.com/prefcanoas" aria-label="Twitter"><i class="fab fa-twitter fa-2x"></i></a>
+            <a class="btn btn-outline-light btn-social mx-2" target="_blank" href="https://www.facebook.com/PrefeituradeCanoas" aria-label="Facebook"><i class="fab fa-facebook-f fa-2x"></i></a>
+            <a class="btn btn-outline-light btn-social mx-2" target="_blank" href="https://www.instagram.com/prefcanoas/" aria-label="Instagram"><i class="fab fa-instagram fa-2x"></i></a>
+          </div>
+        </div>
+        
+        <hr>
+
+        <div class="row footer-link">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 mb-4">
+            <a class="link-dark text-decoration-none me-3 footer-link-item" href="#!">Vagas</a>
+          </div>
+              
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 mb-4">
+            <a class="link-dark text-decoration-none me-3 footer-link-item" href="#!">Política de Privacidade</a>
+          </div>
+              
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 mb-4">
+            <a class="link-dark text-decoration-none me-3 footer-link-item" href="#!">Perguntas Frequentes</a>
+          </div>
+              
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 mb-4">
+            <a class="link-dark text-decoration-none me-3 footer-link-item" href="#!">Fale Conosco</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <div class="bg-canoas  py-2 text-center text-white">
+      <div class="container"><small>Copyright &copy; CanoasTec {{ nowYear }}</small></div>
+    </div>
   </div>
 </template>
 
@@ -126,6 +208,7 @@
 // @ is an alias to /src
 import gsap from 'gsap';
 const axios = require('axios');
+const moment = require('moment');
 export default {
   name: "Home",
   components: {
@@ -138,6 +221,7 @@ export default {
       amountCompany: 0,
       vacancies: [],
       objectives: [],
+      nowYear: moment().format('YYYY'),
     }
   },
   created() {
@@ -150,14 +234,14 @@ export default {
       }.bind(this));
    
    axios.get('https://dsv.pmcanoas.rs.gov.br/bdomobilews/vagas/ultimas-vagas')
-      .then(function (response) {
-        this.vacancies = response.data;
-      }.bind(this));
+    .then(function (response) {
+      this.vacancies = response.data;
+    }.bind(this));
    
    axios.get('https://dsv.pmcanoas.rs.gov.br/bdomobilews/info/objetivo')
-      .then(function (response) {
-        this.objectives = response.data.data;
-      }.bind(this));
+    .then(function (response) {
+      this.objectives = response.data.data;
+    }.bind(this));
   }
 };
 </script>
@@ -185,7 +269,7 @@ export default {
   .carousel-item {
     height: 25rem;
   }
-  .last-vacancies {
+  .last-vacancies, .area-user {
     background: #2B8F6F;
   }
 
@@ -198,10 +282,24 @@ export default {
   .carousel-caption {
     color: #000;
     background-color: #fff;
-    padding: 3em 3em;
+    padding: 1em 3em;
+    height: 90%;
     border-radius: 0.5em;
   }
+  .area-user-item {
+    color: #000;
+    background-color: #fff;
+    height: 20em;
+    border-radius: 0.5em;
+    margin-top: 1em;
+  }
 
+  .carousel-vacancies-description {
+    word-wrap: break-word!important;
+    word-break: break-word!important;
+    height: 22%;
+    overflow: hidden;
+  }
   .carousel-indicators [data-bs-target] {
     margin-bottom: 0;
   }
@@ -216,6 +314,24 @@ export default {
   }
   .information-objective .card-header {
     background-image: linear-gradient(to bottom right, #2B8F6F , #42DBAB);
+    color: #fff;
+  }
+
+  .btn-social {
+    height: 3rem;
+    width: 3rem;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0;
+    border-radius: 100%;
+    background-image: linear-gradient(to bottom right, #2B8F6F , #42DBAB);
+  }
+
+  .footer-link-item:hover {
+    background-image: linear-gradient(to bottom right, #2B8F6F , #42DBAB);
+    padding: 1em 2em;
+    border-radius: 4px;
     color: #fff;
   }
 </style>
